638c14f0f53e66cd335c88ae3710f76964aed1bd
Matt Meyer <matthew.meyer29@gmail.com>
2018-05-01 14:31:05 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express     = require('express')
+const bodyParser  = require('body-parser')
+const morgan      = require('morgan')
+const cors        = require('cors')
+const app         = module.exports = express()
+const port        = parseInt(process.env.PORT || 3000)
+
+const data = {id:1, name:"brewery"}
+
+app.get('/api/v1/breweries', (request, response) => {response.json(data)
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
+app.use(cors({origin: true}))
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+
+console.log("this is working")
+
+
+
+function notFound(req, res, next) {
+  const url = req.originalUrl
+  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+    // Don't log less important (automatic) browser requests
+    console.error('[404: Requested file not found] ', url)
+  }
+  res.status(404).send({error: 'Url not found', status: 404, url})
+}
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
+
+app.listen(port)
+  .on('error',     console.error.bind(console))
+  .on('listening', console.log.bind(console, 'Listening on ' + port));
